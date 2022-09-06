e0772a19c6ce188437ebaccf5a69edfa16b62ba7
oleg khrulev <linqur@gmail.com>
2022-09-06 12:38:36 +0300

    1

--- craftassistent/craftassistent.lua
+++ craftassistent/craftassistent.lua
@@ -1,77 +1,95 @@
-_autocrafter = { 
+_craft_assistent = { 
     runing = false, 
     init = false, 
     selling = false 
-   } 
-    
-   function _autocrafter:OnEvent(event, ...) 
-    if _autocrafter.runing == true then 
-     _autocrafterevent 
-    end 
-   end 
-    
-   function _autocrafter:BAG_UPDATE () 
-    if _autocrafter.selling ~= true and _autocrafter:isCraftDone() then 
-     _autocrafter:sell() 
-    end 
-   end 
-    
-   function _autocrafter:start (craft_id, crafting_item_id, count) 
-    _autocrafter.craft_id = craft_id 
-    _autocrafter.count = count 
-    _autocrafter.crafting_item_id = crafting_item_id; 
-    _autocrafter.craft_info = C_TradeSkillUI.GetRecipeInfo(craft_id) 
-    
-    if _autocrafter.init ~= true then 
-     _autocrafter:init() 
+} 
+
+function _craft_assistent:start(craft_id, crafting_item_id, count) 
+    _craft_assistent.craft_id = craft_id
+    _craft_assistent.crafting_item_id = crafting_item_id
+    _craft_assistent.count = count
+
+    if _craft_assistent.init ~= true then 
+        _craft_assistent:init() 
     end 
     
-    if _autocrafter.runing == false then 
-     _autocrafter.runing = true 
-     _autocrafter:craft() 
+    if _craft_assistent.runing ~= true then 
+        _craft_assistent.runing = true 
+        _craft_assistent:craft()
     else 
-     _autocrafter:stop() 
+        _craft_assistent:stop() 
     end 
-   end 
+end 
+
+function _craft_assistent:init()
+    _craft_assistent.frame = CreateFrame("Frame") 
+    _craft_assistent.frame:SetScript("OnEvent", _craft_assistent.OnEvent) 
+    _craft_assistent.frame:RegisterEvent("BAG_UPDATE", _craft_assistent.OnEvent) 
+    _craft_assistent.init = true; 
+end 
+
+function _craft_assistent:stop()
+    _craft_assistent.runing = false
+    _craft_assistent.selling = false
+end 
+
+function _craft_assistent:craft()
+    C_TradeSkillUI.CraftRecipe(_craft_assistent.craft_id, _craft_assistent.count) 
+end
     
-   function _autocrafter:craft () 
-    C_TradeSkillUI.CraftRecipe(_autocrafter.craft_id, _autocrafter.count) 
-   end 
-    
-   function _autocrafter:isCraftDone() 
+function _craft_assistent:isCraftDone()
     local count = 0 
     for bag = 0, 4, 1 do  
-     for slot = 1, GetContainerNumSlots(bag), 1 do  
-      if GetContainerItemID(bag, slot) == _autocrafter.crafting_item_id then  
-       count = count + 1 
-      end 
-     end 
+        for slot = 1, GetContainerNumSlots(bag), 1 do  
+            if GetContainerItemID(bag, slot) == _craft_assistent.crafting_item_id then  
+                count = count + 1 
+            end 
+        end 
     end 
-    return count >= _autocrafter.count 
-   end 
+    return count >= _craft_assistent.count 
+end 
     
-   function _autocrafter:sell() 
-    _autocrafter.selling = true; 
+function _craft_assistent:sell()
     for bag = 0, 4, 1 do  
-     for slot = 1, GetContainerNumSlots(bag), 1 do  
-      if GetContainerItemID(bag, slot) == _autocrafter.crafting_item_id then  
-       UseContainerItem(bag, slot) 
-      end 
-     end 
+        for slot = 1, GetContainerNumSlots(bag), 1 do  
+            if GetContainerItemID(bag, slot) == _craft_assistent.crafting_item_id then  
+                UseContainerItem(bag, slot)
+                return true
+            end 
+        end 
     end 
-    _autocrafter.selling = false; 
-   end 
+end 
     
-   function _autocrafter:init() 
-    _autocrafter.frame = CreateFrame("Frame") 
-    _autocrafter.frame:SetScript("OnEvent", _autocrafter.OnEvent) 
-    _autocrafter.frame:RegisterEvent("BAG_UPDATE", _autocrafter.OnEvent) 
-    _autocrafter.init = true; 
-   end 
+function _craft_assistent:OnEvent(event, ...)
+    if _craft_assistent.runing == true then 
+        _craft_assistent[event](...) 
+    end 
+end 
     
-   function _autocrafter:stop() 
-    _autocrafter.runing = false 
-   end 
-    
-   print(132)
+function _craft_assistent:BAG_UPDATE()
+    if _craft_assistent.selling ~= true and _craft_assistent:isCraftDone() then 
+        _craft_assistent.selling = true;
+    end
+
+    if _craft_assistent.selling == true then
+        if _craft_assistent:haveItemToSell() then
+            _craft_assistent:sell()
+        else
+            _craft_assistent:stop()
+        end
+    end
+end 
+
+function _craft_assistent:haveItemToSell()
+    for bag = 0, 4, 1 do  
+        for slot = 1, GetContainerNumSlots(bag), 1 do  
+            if GetContainerItemID(bag, slot) == _craft_assistent.crafting_item_id then
+                return true
+            end 
+        end 
+    end 
+    return false
+end
+
+print(132)
    
