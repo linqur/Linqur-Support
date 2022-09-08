09811f72fa008e35f673960dae7966213945d6b0
linqur <Linqur@gmail.com>
2022-09-08 09:55:36 +0300

    Update LinqurSupport.lua

--- LinqurSupport/LinqurSupport.lua
+++ LinqurSupport/LinqurSupport.lua
@@ -1,68 +1,21 @@
-_linqur_suport.craft_and_sell.sellLimit = 7
-_linqur_suport.craft_and_sell.sellingSlots = {}
+_linqur_suport = {}
 
-function _linqur_suport.craft_and_sell:sell()
-    local nothingToSell = true
+function _linqur_suport:printMessage(message)
+    print("[Linqur Support]: " .. message)
+end
 
-    _linqur_suport.craft_and_sell:clearSelledSlots()
-    
-    for bag = 0, 4, 1 do
-        for slot = 1, GetContainerNumSlots(bag), 1 do
-            if _linqur_suport.craft_and_sell:needlyToSell(bag, slot) then 
-                nothingToSell = false
-                if _linqur_suport.craft_and_sell:getSellingSlotsLength() < _linqur_suport.craft_and_sell.sellLimit then
-                    _linqur_suport.craft_and_sell:addToSell(bag, slot)
-                end
-            end
-        end 
+function _linqur_suport:getItemIdByItemLink(itemLink)
+    if itemLink == nil then
+        return nil
     end
 
-    if nothingToSell then
-        _linqur_suport.craft_and_sell:sellDone()
+    local from, to = string.find(itemLink, "item:%d+")
+
+    if from == nil then
+        return nil
     end
-end
 
-function _linqur_suport.craft_and_sell:needlyToSell(bag, slot)
-	local id = GetContainerItemID(bag, slot)
-    return id ~= nil and id == _linqur_suport.craft_and_sell.crafted_item_id
-end
+    local match = string.sub(itemLink, from, to)
 
-function _linqur_suport.craft_and_sell:addToSell(bag, slot)
-    _linqur_suport.craft_and_sell.sellingSlots[bag .. '_' .. slot] = {
-        bag = bag,
-        slot = slot
-    }
-    UseContainerItem(bag, slot)
-end
-
-function _linqur_suport.craft_and_sell:getSellingSlotsLength()
-    local count = 0
-    for k, v in pairs(_linqur_suport.craft_and_sell.sellingSlots) do
-        count = count + 1
-    end
-    return count
-end
-
-function _linqur_suport.craft_and_sell:clearSelledSlots()
-    local deleteKeys = {}
-    for k, v in pairs(_linqur_suport.craft_and_sell.sellingSlots) do
-        if _linqur_suport.craft_and_sell:isSelledSlot(v.bag, v.slot) then
-            deleteKeys[k] = k
-        end
-    end
-    for k, v in pairs(deleteKeys) do
-        _linqur_suport.craft_and_sell.sellingSlots[k] = nil
-    end
-end
-
-function _linqur_suport.craft_and_sell:isSelledSlot(bag, slot)
-    return GetContainerItemID(bag, slot) == nil
-end
-
-function _linqur_suport.craft_and_sell.events:BAG_UPDATE()
-	if _linqur_suport.craft_and_sell.action ~= 'sell' then
-		return
-	end
-
-	_linqur_suport.craft_and_sell:sell()
+    return string.gsub(match, 'item:', '') + 0
 end
