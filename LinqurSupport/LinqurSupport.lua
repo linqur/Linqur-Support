f81a8087edc159e7aab905f59201231b54217678
linqur <Linqur@gmail.com>
2022-09-08 09:53:03 +0300

    Update LinqurSupport.lua

--- LinqurSupport/LinqurSupport.lua
+++ LinqurSupport/LinqurSupport.lua
@@ -3,3 +3,19 @@ _linqur_suport = {}
 function _linqur_suport:printMessage(message)
     print("[Linqur Support]: " .. message)
 end
+
+function _linqur_suport:getItemIdByItemLink(itemLink)
+    if itemLink == nil then
+        return nil
+    end
+
+    local from, to = string.find(itemLink, "item:%d+")
+
+    if from == nil then
+        return nil
+    end
+
+    local match = string.sub(itemLink, from, to)
+
+    return string.gsub(match, 'item:', '') + 0
+end
